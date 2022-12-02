f9ffb2cbddae04b76b490fab6d50ace9623e7eb7
Rémy <remyc59440@hotmail.com>
2022-12-02 09:18:19 +0100

    fini

---
+++ exo.js
@@ -0,0 +1,50 @@
+let one = 34.589456;
+
+let two = 68.412784;
+
+one = Math.floor(one);
+console.log(one)
+
+two = Math.floor(two);
+console.log(two)
+
+let three = 39.456
+let four = 27.34876541
+
+
+three = Math.ceil(three);
+console.log(three)
+
+four = Math.ceil(four);
+console.log(four)
+
+let five = 5.68
+five = Math.round(Math.floor(five));
+console.log(five)
+
+let six = 5.41
+six = Math.round(Math.ceil(six))
+console.log(six)
+
+let sept = 6.12457
+sept = parseInt(sept)
+console.log(sept)
+
+let heiht = 9.568124
+heiht = parseInt(heiht)
+console.log(heiht)
+
+let nine = 9.568124
+heiht = parseInt(nine)
+console.log(nine)
+
+let a = 5, b = 1, c = 12, d = 1, e = 45;
+let min =  Math.min(a,b,c,d,e)
+console.log(min)
+let t = 9, r = 12, f = 16, g = 44, h =3;
+let max =  Math.max(t,r,f,g,h)
+console.log(max)
+
+let abs = -56;
+abs = Math.abs(abs);
+console.log(abs)
